7d623b5d5f035d6304f7d7c6f54e12a6d9a84389
Lucian Miller <lucian.miller185@gmail.com>
2021-10-04 10:48:12 -0700

    create initial project structure

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Weekday Calculator',
       template: './src/index.html',
       inject: 'body'
     })

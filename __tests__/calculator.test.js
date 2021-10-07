d9035953b7ae540097ec3ecf9ecf0b85048dc16d
Lucian Miller <lucian.miller185@gmail.com>
2021-10-07 10:46:22 -0700

    Add failing test for WeekdayCalculator object

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,9 @@
+import WeekdayCalculator from "../src/calculator";
+
+describe('WeekdayCalculator', () => {
+
+  test('should correctly create a WeekdayCalculator object that stores the outcome of a Date object', () => {
+    let day = new WeekdayCalculator(5, 25, 2021);
+    expect(day.date).toEqual(new Date(2021, 5, 25));
+  })
+})

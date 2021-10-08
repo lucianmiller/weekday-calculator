edf097096bd5ee35d897b6edfb9c5f75daeb3c3f
Lucian Miller <lucian.miller185@gmail.com>
2021-10-08 11:25:57 -0700

    Add DayOfWeek function

--- src/calculator.js
+++ src/calculator.js
@@ -2,4 +2,9 @@ export default class WeekdayCalculator {
   constructor(month, day, year) {
     this.date = new Date(year, month, day);
   }
+
+  DayOfTheWeek() {
+    let weekdayNumValue = this.date.getDay();
+    return weekdayNumValue;
+  }
 }

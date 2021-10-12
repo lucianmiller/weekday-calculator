28165cd4d3b9a12f80e5be08d8564412fcbb6aa0
Lucian Miller <lucian.miller185@gmail.com>
2021-10-12 10:57:00 -0700

    fix inconsistencies

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,18 +3,18 @@ import WeekdayCalculator from "../src/calculator";
 describe('WeekdayCalculator', () => {
 
   test('should correctly create a WeekdayCalculator object that stores the outcome of a Date object', () => {
-    let day = new WeekdayCalculator(5, 25, 2021);
+    let day = new WeekdayCalculator(6, 25, 2021);
     expect(day.date).toEqual(new Date(2021, 5, 25));
   })
 
   test('should return a numerical value for a weekday.', () => {
-    let day = new WeekdayCalculator(5, 25, 2021);
+    let day = new WeekdayCalculator(6, 25, 2021);
     const weekday = day.DayOfTheWeek();
     expect(weekday).toEqual(5);
   })
 
   test('should return sunday if day equals 0.', () => {
-    let day = new WeekdayCalculator(10, 10, 2021);
+    let day = new WeekdayCalculator(10, 3, 2021);
     const weekday = day.DayOfTheWeek();
     const weekdayString = day.WhichDay(weekday);
     expect(weekdayString).toEqual("Sunday");

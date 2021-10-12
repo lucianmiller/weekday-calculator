fa8bfcfd1cf5bb44d53fe27351c765b040e7ca2f
Lucian Miller <lucian.miller185@gmail.com>
2021-10-12 10:57:39 -0700

    add WhichDay prototype and fix off by one errors

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,16 @@
 export default class WeekdayCalculator {
   constructor(month, day, year) {
-    this.date = new Date(year, month, day);
+    this.date = new Date(year, (month - 1), day);
   }
 
   DayOfTheWeek() {
     let weekdayNumValue = this.date.getDay();
     return weekdayNumValue;
+  }
+
+  WhichDay(day) {
+    if (day === 0) {
+      return "Sunday";
+    }
   }
 }

895b8a48a43808d3a91252ee4626333f8174ba36
Lucian Miller <lucian.miller185@gmail.com>
2021-10-13 11:11:18 -0700

    add else if for thursday

--- src/calculator.js
+++ src/calculator.js
@@ -17,6 +17,8 @@ export default class WeekdayCalculator {
       return "Tuesday";
     } else if (day === 3) {
       return "Wednesday";
+    } else if (day ===4) {
+      return "Thursday";
     }
   }
 }
